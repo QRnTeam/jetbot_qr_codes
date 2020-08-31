d7ff846e68fbd97f845cdeb8aa035f09bceadd39
Nathan Perkins <nperkins487@gmail.com>
2020-08-30 17:37:22 -0700

    init aruco.py

--- src/aruco.py
+++ src/aruco.py
@@ -1,13 +1,16 @@
 #!/usr/bin/env python
 
 import rospy
-import numpy as np
-from .vision.ArUcoReader import ArucoReader
-
+import cv_bridge
 from sensor_msgs.msg import Image
+
+import numpy as np
+from .vision.arucoreader import ArucoReader
 
 mtx = rospy.get_param('camera_matrix')
 dist = rospy.get_param('camera_dist')
+aruco_side_length = rospy.get_param('aruco_side_length')
+
 raw_camera_topic = rospy.get_param('raw_camera_topic')
 
 camera_matrix = np.mat(camera_matrix)
@@ -15,8 +18,15 @@ camera_dist = np.mat(camera_dist)
 
 class MarkersTopic(object):
     def __init__(self):
-        self._reader = ArucoReader(mtx, dist)
+        self._reader = ArucoReader(mtx, dist, aruco_side_length)
         self._image_sub = rospy.Subscriber(raw_camera_topic, Image, self.image_callback)
 
     def image_callback(self, image):
-        pass
+        image = cv_bridge.CvBridge().imgmsg_to_cv2(image)
+        cv2.imshow('live', image)
+        cv2.waitKey(0)
+
+if __name__ == '__main__':
+    rospy.init_node("arucos")
+    MarkersTopic()
+    rospy.spin()
