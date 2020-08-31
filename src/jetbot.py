7424e0e781dbc1ba8a936d7ea539f95c2ec41513
Nathan Perkins <nperkins487@gmail.com>
2020-08-30 21:12:39 -0700

    init jetbot.py

---
+++ src/jetbot.py
@@ -0,0 +1,129 @@
+#!/usr/bin/env python
+import rospy
+
+from geometry_msgs.msg import Twist
+from jetbot_qr_codes.msg import Markers
+
+LIMITER_SIGNS = {
+    #id: [front_limit, back_limit]
+    0: (0.00, 0.00),
+    1: (0.50, 0.50),
+    2: (1.00, 1.00),
+    # 3, 4 are reserved for special signs
+}
+
+def abs_clamp(val, _max):
+    if val >= 0:
+        val = min(_max, val)
+    else:
+        val = max(-_max, val)
+
+    return val
+
+cmd_vel_topic = rospy.get_param('cmd_vel_topic')
+markers_topic = rospy.get_param('markers_topic')
+cmd_raw_topic = rospy.get_param('cmd_raw_topic')
+
+class Jetbot(object):
+    def __init__(self, wheelbase, wheel_radius):
+        self._wheelbase_meters = wheelbase
+        self._wheel_radius_meters = wheel_radius
+        self._max_speed = 0.2
+
+        self._rate = rospy.Rate(5)
+
+        self._twist = Twist()
+        self._vel_sub = rospy.Subscriber(cmd_vel_topic, Twist, self.vel_callback)
+
+        self._markers = []
+        self._markers_sub = rospy.Subscriber(markers_topic, Markers, self.markers_callback)
+        
+        # TODO: Move cmd_raw into this class.
+        self._raw_pub = rospy.Publisher(cmd_raw_topic, String, queue_size=10)
+
+    def vel_callback(self, twist):
+        rospy.loginfo("Received cmd_vel: {}".format(twist))
+        self._twist = twist
+
+    def markers_callback(self, markers):
+        rospy.loginfo("Received markers: {}".format(markers))
+        markers = markers.markers
+        markers.sort(key=lambda x: x.position[2])
+        self._markers = markers
+
+    def run(self):
+        while not rospy.is_shutdown():
+            twist = self._twist
+            vel = twist.linear.x
+
+            # clamp to max speed
+            vel = abs_clamp(vel, self._max_speed)
+
+            # find speed limit from closest marker
+            if np.any(self._markers):
+                marker = self._markers[0]
+                front_limit, back_limit = LIMITER_SIGNS.get(marker.id, (0, 0))
+            else:
+                front_limit, back_limit = (0, 0)
+
+            # apply speed limit based on max speed
+            if vel >= 0:
+                vel = abs_clamp(vel, front_limit*self._max_speed)
+            else:
+                vel = abs_clamp(vel, back_limit*self._max_speed)
+
+            twist.linear.x = vel
+            self.move(twist)
+            self._rate.sleep()
+
+    def move(self, twist):
+        # Apply twist to two-wheeled differential drive robot.
+        #
+        # The output will be published to cmd_raw, which accepts the left 
+        # and right motor power encoded as a comma-separated string.
+        #
+        # Example: "-1.0,0.5"
+        #   (full reverse for left and half forward for right)
+        v_l, v_r = self.unicycle(twist)
+        speed_l, speed_r = abs(v_l), abs(v_r)
+
+        if speed_l > 1.0 and speed_l > speed_r:
+            v_l /= speed_l
+            v_r /= speed_l
+        elif speed_r > 1.0:
+            v_l /= speed_r
+            v_r /= speed_r
+
+        cmd = "{},{}".format(v_l, v_r)
+        self._raw_pub.publish(cmd)
+
+    def unicycle(self, twist):
+        # Apply unicycle model to two-wheeled differential drive robot.
+        #
+        # From http://faculty.salina.k-state.edu/tim/robotics_sg/Control/kinematics/unicycle.html
+        #
+        # Given:
+        #   v: twist.linear.x   (in meters per second)
+        #   w: twist.angular.z  (in radians per second)
+        #   L: wheelbase width  (in meters per radian)
+        #   R: wheel radius     (in meters per radian)
+        #
+        # Calculate:
+        #   v_r, v_l: velocity for left and right wheel
+        # 
+        # Equations:
+        #   v_r = ((2 * v) + (w * L)) / (2 * R)
+        #   v_l = ((2 * v) - (w * L)) / (2 * R)
+        v = twist.linear.x
+        w = twist.angular.z
+        L = self._wheelbase_meters
+        R = self._wheel_radius_meters
+
+        v_l = ((2 * v) - (w * L)) / (2 * R)
+        v_r = ((2 * v) + (w * L)) / (2 * R)
+        return v_l, v_r
+
+if __name__ == '__main__':
+    rospy.init_node('jetbot')
+    Jetbot()
+    rospy.spin()
